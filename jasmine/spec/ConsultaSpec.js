d0be2cf189a435b43cc19542b4944d3d2e3c5269
Lucas Gigek <lucasgigek@gmail.com>
2018-01-03 16:58:39 -0200

    Terceira aula

--- jasmine/spec/ConsultaSpec.js
+++ jasmine/spec/ConsultaSpec.js
@@ -1,31 +1,40 @@
 describe("Consulta", function(){
 
-    it("não deve cobrar se for um retorno", function(){
-        var paciente = new Paciente("teste", 50, 80, 1.80);
-        var consulta = new Consulta(consulta, [], true, true);
+    var paciente;
 
-        expect(consulta.preco()).toEqual(0);
+    beforeEach(function(){
+        // paciente = new PacienteBuilder.comPeso(90).constroi();
+        paciente = new PacienteBuilder().constroi();
+    })
+    
+    describe("consultas com tipo retorno", function(){
+        it("não deve cobrar se for um retorno", function(){
+            var consulta = new Consulta(paciente, [], true, true);
+
+            expect(consulta.preco()).toEqual(0);
+        });
     });
 
-    it("deve cobrar 25 para cada procedimento comum", function(){
-        var paciente = new Paciente("teste", 50, 80, 1.80);
-        var consulta = new Consulta(consulta, ["proc-comum-1","proc-comum-2"], false, false);
+    describe("consultas com procedimento", function(){
 
-        expect(consulta.preco()).toEqual(50);
-    });
+        it("deve cobrar 25 para cada procedimento comum", function(){
+            var consulta = new Consulta(paciente, ["proc-comum-1","proc-comum-2"], false, false);
 
-    it("deve cobrar o preço para cada procecimento específico", function(){
-        var paciente = new Paciente("teste", 50, 80, 1.80);
-        var consulta = new Consulta(consulta, ["proc-comum-1","raio-x","gesso"], false, false);
+            expect(consulta.preco()).toEqual(50);
+        });
 
-        expect(consulta.preco()).toEqual(25+55+32);
-    });
+        it("deve cobrar o preço para cada procecimento específico", function(){
+            var consulta = new Consulta(paciente, ["proc-comum-1","raio-x","gesso"], false, false);
 
-    it("deve cobrar o dobro para consultas particulares", function(){
-        var paciente = new Paciente("teste", 50, 80, 1.80);
-        var consulta = new Consulta(consulta, ["proc-comum-1"], true, false);
+            expect(consulta.preco()).toEqual(25+55+32);
+        });
 
-        expect(consulta.preco()).toEqual(25*2);
+        it("deve cobrar o dobro para consultas particulares", function(){
+            var consulta = new Consulta(paciente, ["proc-comum-1"], true, false);
+
+            expect(consulta.preco()).toEqual(25*2);
+        });
+
     });
 
 });

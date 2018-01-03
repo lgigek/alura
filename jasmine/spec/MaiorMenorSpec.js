fdec6d841f1c5d72fee09a1097a52b98def5cfc9
Lucas Gigek <lucasgigek@gmail.com>
2018-01-03 10:14:02 -0200

    Início do curso de Jasmine

---
+++ jasmine/spec/MaiorMenorSpec.js
@@ -0,0 +1,27 @@
+describe("Maior e Menor", function(){
+
+    it("deve retornar maior e menor com os números ordem não sequencial", function(){
+        var algoritmo = new MaiorMenor();
+        algoritmo.encontra([10,5,15,2]);
+
+        expect(algoritmo.pegaMaior()).toEqual(15);
+        expect(algoritmo.pegaMenor()).toEqual(2);
+    });
+
+    it("deve retornar maior e menor com os números ordem crescente", function(){
+        var algoritmo = new MaiorMenor();
+        algoritmo.encontra([1,2,3,4]);
+
+        expect(algoritmo.pegaMaior()).toEqual(4);
+        expect(algoritmo.pegaMenor()).toEqual(1);
+    });
+
+    it("deve retornar maior e menor com os números ordem decrescente", function(){
+        var algoritmo = new MaiorMenor();
+        algoritmo.encontra([9,8,7,6]);
+
+        expect(algoritmo.pegaMaior()).toEqual(9);
+        expect(algoritmo.pegaMenor()).toEqual(6);
+    });
+
+});

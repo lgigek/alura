b752cae841603061bacc6027844455797160f35e
Lucas Gigek <lucasgigek@gmail.com>
2018-02-07 00:52:14 -0200

    Quarta aula Inclusão do Filter FiltroDeAuditoria para logar as URIs acessadas

---
+++ servlet/gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = "/*")
+public class FiltroDeAuditoria implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uri = req.getRequestURI();
+		System.out.println("Usuário acessando a URI: " + uri);
+		
+		// Para seguir com a requisição após realizar o Filter
+		chain.doFilter(request, response);
+	}
+
+}

b55941bd9df6c56aa890dacf5931214bf38af63c
Lucas Gigek <lucasgigek@gmail.com>
2018-02-08 22:33:01 -0200

    Aula 5 Inclusão da classe Login (uri=/login) Criação e manipulação de cookie

--- servlet/gerenciador/src/main/java/br/com/alura/gerenciador/dao/UsuarioDAO.java
+++ servlet/gerenciador/src/main/java/br/com/alura/gerenciador/dao/UsuarioDAO.java
@@ -11,6 +11,7 @@ public class UsuarioDAO {
 	static {
 		USUARIOS.put("guilherme.silveira@alura.com.br", new Usuario("guilherme.silveira@alura.com.br","silveira"));
 		USUARIOS.put("rodrigo.turini@alura.com.br", new Usuario("rodrigo.turini@alura.com.br","turini"));
+		USUARIOS.put("fred@sedeubem.com", new Usuario("fred@sedeubem.com", "valeuhein"));
 	}
 
 	public Usuario buscaPorEmailESenha(String email, String senha) {
---
+++ servlet/gerenciador/src/main/java/br/com/alura/gerenciador/web/Login.java
@@ -0,0 +1,38 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Usuario;
+import br.com.alura.gerenciador.dao.UsuarioDAO;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+
+		String email = req.getParameter("email");
+		String senha = req.getParameter("senha");
+
+		Usuario usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
+
+		if (usuario == null)
+			writer.println("<html><body><h1>Usuário inválido</h1></body></html>");
+		else {
+			Cookie cookieLogado = new Cookie("usuario.logado", usuario.getEmail());
+			resp.addCookie(cookieLogado);
+			
+			writer.println("<html><body><h1>Usuário logado: " + usuario.getEmail() + " </h1></body></html>");
+		}
+	}
+
+}

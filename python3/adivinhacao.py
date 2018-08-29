f9d8f5c739e83f690bec63627942417f19f63945
Lucas Gigek <lucasgigek@gmail.com>
2018-08-28 22:25:43 -0300

    Inicio do curso de Python 3 - Aula 2

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -7,7 +7,15 @@ numero_secreto = 10
 numero_usuario = int(input("Digite o seu número: "))
 print("Você digitou o número", numero_usuario)
 
-if (numero_secreto == numero_usuario):
-    print("Você acertou")
+acertou = numero_secreto == numero_usuario
+eh_maior = numero_secreto < numero_usuario
+eh_menor = numero_secreto > numero_usuario
+
+if acertou:
+    print("Você acertou!")
 else:
-    print("Você errou")
+    if eh_maior:
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif eh_menor:
+        print("Você errou! O seu chute foi menor que o número secreto.")
+print("Fim do jogo")

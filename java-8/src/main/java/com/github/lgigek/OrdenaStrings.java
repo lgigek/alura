a9cbf12ccb34abb46753accf5ff5c8801ff254bb
Lucas Gigek <lucasgigek@gmail.com>
2018-11-12 14:41:17 -0200

    Java 8 - Aula 2

--- java-8/src/main/java/com/github/lgigek/OrdenaStrings.java
+++ java-8/src/main/java/com/github/lgigek/OrdenaStrings.java
@@ -15,33 +15,9 @@ public class OrdenaStrings {
         palavras.add("editora casa do código");
         palavras.add("caelum");
 
-        Comparator<String> comparator = new ComparadorPorTamanho();
-        palavras.sort(comparator);
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
         System.out.println(palavras);
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        palavras.forEach(consumidor);
-    }
-}
-
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-
-        if (s1.length() < s2.length())
-            return -1;
-        if (s1.length() > s2.length())
-            return 1;
-        return 0;
-    }
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
+        palavras.forEach(s -> System.out.println(s));
     }
 }
